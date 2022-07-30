1a453686366b36362d174470f0d388119098eebb
FlaringK <caelans@iinet.net.au>
2022-07-30 11:35:27 +0800

    Set even more colours
    God why are there so many

--- Homestuckify.lua
+++ Homestuckify.lua
@@ -20,22 +20,68 @@ ModUtil.WrapBaseFunction(
     end
 )
 
+--!! Set Colours
+
+homestuckColors = {
+	June = { 7, 21, 205, 255 },
+	Rose = { 181, 54, 218, 255 },
+	Dave = { 224, 7, 7, 255 },
+	Jade = { 74, 201, 37, 255 },
+	Jane = { 0, 213, 242, 255 },
+	Jake = { 31, 148, 0, 255 },
+	Roxy = { 241, 65, 239, 255 },
+	Dirk = { 242, 164, 0, 255 },
+	Vriska = { 0, 86, 130, 255 },
+	Eridan = { 106, 0, 106, 255 },
+
+	SpaceBg = { 0, 0, 0, 255 },
+	SpaceAp = { 255, 255, 255, 255 },
+	LightBg = { 247, 250, 78, 255 },
+	LightAp = { 247, 250, 78, 255 },
+	VoidBg = { 3, 52, 118, 255 },
+	VoidAp = { 0, 19, 75, 255 },
+	LifeBg = { 204, 195, 180, 255 },
+	LifeAp = { 118, 195, 78, 255 },
+	HopeBg = { 255, 224, 148, 255 },
+	HopeAp = { 253, 253, 253, 255 },
+	TimeBg = { 183, 13, 14, 255 },
+	TimeAp = { 255, 35, 6, 255 },
+	HeartBg = { 85, 20, 42, 255 },
+	HeartAp = { 189, 24, 100, 255 },
+	BreathBg = { 67, 121, 230, 255 },
+	BreathAp = { 16, 223, 255, 255 },
+	RageBg = { 67, 121, 230, 255 },
+	RageAp = { 16, 223, 255, 255 },
+}
+
+--Format Lookup
+homestuckLookup = {
+	Aphrodite = "Roxy",
+	Ares = "Jane",
+	Artemis = "Dirk",
+	Athena = "Rose",
+	Chaos = "Eridan",
+	Demeter = "Dave",
+	Dionysus = "Jake",
+	Hermes = "June",
+	Poseidon = "Vriska",
+	Zeus = "Jade"
+}
+
+homestuckGodKidPairs = { -- This doesn't include Jade, Eridan and Vriska since they have unique conditions
+	June = "Breath",
+	Rose = "Light",
+	Dave = "Time",
+	Jane = "Life",
+	Jake = "Hope",
+	Roxy = "Void",
+	Dirk = "Heart",
+}
+
+
 --!! Add new Text formats for Charaters
 
 local function addTextFormatting()
-
-	homestuckColors = {
-		June = { 7, 21, 205, 255 },
-		Rose = { 181, 54, 218, 255 },
-		Dave = { 224, 7, 7, 255 },
-		Jade = { 74, 201, 37, 255 },
-		Jane = { 0, 213, 242, 255 },
-		Jake = { 31, 148, 0, 255 },
-		Roxy = { 241, 65, 239, 255 },
-		Dirk = { 242, 164, 0, 255 },
-		Vriska = { 0, 86, 130, 255 },
-		Eridan = { 106, 0, 106, 255 }
-	}
 
 	-- Base text, change font and shit here
 	baseFormatTable = {
@@ -78,20 +124,6 @@ end
 -- Run this AFTER replacing all the text but BEFORE replacing the audio
 
 local function applyFormattingDeep(topTable)
-
-	--Format Lookup
-	homestuckLookup = {
-		Aphrodite = "Roxy",
-		Ares = "Jane",
-		Artemis = "Dirk",
-		Athena = "Rose",
-		Chaos = "Eridan",
-		Demeter = "Dave",
-		Dionysus = "Jake",
-		Hermes = "June",
-		Poseidon = "Vriska",
-		Zeus = "Jade"
-	}
 
 	-- Check if there are deeper tables
 	local hasDeeperTable = false
@@ -137,7 +169,6 @@ end
 ModUtil.DebugCall( function()
 	addTextFormatting()
 	applyFormattingDeep(LootData)
-	applyFormattingDeep(NPCData.PresetEventArgs)
 	applyFormattingDeep(DeathLoopData) -- * This is really inefficent find the specific table later
 	applyFormattingDeep(RoomSetData.Tartarus)
 end )		
@@ -175,3 +206,70 @@ Codex.megido = {
 }
 
 --!! Change FX colours
+
+newGodDmgVoiceColors = {
+	ZeusDamageLight = homestuckColors.SpaceBg,
+	ZeusDamage = homestuckColors.Jade,
+	AthenaDamageLight = homestuckColors.LightBg,
+	AthenaDamage = homestuckColors.LightAp,
+	AphroditeDamageLight = homestuckColors.VoidBg,
+	AphroditeDamage = homestuckColors.VoidAp,
+	AresDamageLight = homestuckColors.LifeBg,
+	AresDamage = homestuckColors.LifeAp,
+	DionysusDamageLight = homestuckColors.HopeBg,
+	DionysusDamage = homestuckColors.HopeAp,
+	PoseidonDamageLight = homestuckColors.Vriska,
+	PoseidonDamage = homestuckColors.Vriska,
+	DemeterDamageLight = homestuckColors.TimeBg,
+	DemeterDamage = homestuckColors.TimeAp,
+	ArtemisDamageLight = homestuckColors.HeartBg,
+	ArtemisDamage = homestuckColors.HeartAp,
+
+	ZeusVoice = homestuckColors.Jade,
+	PoseidonVoice = homestuckColors.Vriska,
+	AthenaVoice = homestuckColors.Rose,
+	AresVoice = homestuckColors.Jane,
+	AphroditeVoice = homestuckColors.Roxy,
+	ArtemisVoice = homestuckColors.Dirk,
+	DionysusVoice = homestuckColors.Jake,
+	DemeterVoice = homestuckColors.Dave,
+	ChaosVoice = homestuckColors.Eridan
+}
+
+ModUtil.Table.Merge( Color, newGodDmgVoiceColors )
+
+-- Load Loottable
+
+function createColorSubLootData (Col1, Col2, Col3, Col4)
+	return {
+		Color = homestuckColors[Col1],
+		LightingColor = homestuckColors[Col2],
+		LootColor = homestuckColors[Col3],
+		SubtitleColor = homestuckColors[Col4]
+	}
+end
+
+function createAspectColorSubLootData (kidName, aspectName)
+	return createColorSubLootData(kidName, aspectName .. "Ap", aspectName .. "Bg", kidName)
+end
+
+function createTextOnlyColorSubLootData (kidName)
+	return createColorSubLootData(kidName, kidName, kidName, kidName)
+end
+
+ModUtil.DebugCall( function()
+	ModUtil.Table.Merge( LootData.HermesUpgrade, createAspectColorSubLootData("June", "Breath") )
+	ModUtil.Table.Merge( LootData.AthenaUpgrade, createAspectColorSubLootData("Rose", "Light") )
+	ModUtil.Table.Merge( LootData.DemeterUpgrade, createAspectColorSubLootData("Dave", "Time") )
+	ModUtil.Table.Merge( LootData.AresUpgrade, createAspectColorSubLootData("Jane", "Life") )
+	ModUtil.Table.Merge( LootData.DionysusUpgrade, createAspectColorSubLootData("Jake", "Hope") )
+	ModUtil.Table.Merge( LootData.AphroditeUpgrade, createAspectColorSubLootData("Roxy", "Void") )
+	ModUtil.Table.Merge( LootData.ArtemisUpgrade, createAspectColorSubLootData("Dirk", "Heart") )
+	ModUtil.Table.Merge( LootData.TrialUpgrade, createAspectColorSubLootData("Eridan", "Rage") )
+
+	ModUtil.Table.Merge( LootData.ZeusUpgrade, createTextOnlyColorSubLootData("Jade") )
+	ModUtil.Table.Merge( LootData.PoseidonUpgrade, createTextOnlyColorSubLootData("Vriska") )
+
+	print(LootData.ZeusUpgrade.LightingColor[1])
+end)
+
